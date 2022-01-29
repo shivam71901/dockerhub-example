df186514bebc289f85dffcb83e887a3a4332f570
shivam <shivamsn363@gmail.com>
2022-01-29 22:43:12 +0530

    final jenkins commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t narendra3425/webserver .
+docker build -t shivam7190docker/webserver3 .
--- scripts/push.sh
+++ scripts/push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker push narendra3425/webserver
+docker push shivam7190docker/webserver3
